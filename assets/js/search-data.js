bf58fe1e82b41a844e20ec308eefd8973982004e
khyeonn <maxkim7100@gmail.com>
2026-03-12 10:37:26 +0000

    Deploying to gh-pages from @ khyeonn/khyeonn.github.io@d9f02bea83513ccf3ad174c06a1f932221963b05 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,23 +41,7 @@ ninja.data = [{
             window.location.href = "/blog/2026/odrone-1/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-assistive-utensil-for-tremor-patients",
+      },{id: "projects-assistive-utensil-for-tremor-patients",
           title: 'Assistive Utensil for Tremor Patients',
           description: "",
           section: "Projects",handler: () => {
